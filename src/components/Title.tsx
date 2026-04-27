059a887298d491ec9a0dc8b87cb4fc23ac6b61e8
onurtvb <bruno73745@gmail.com>
2026-04-27 21:47:20 +0200

    c

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -2,7 +2,7 @@ export default function Title() {
 
     return (
         <div className="flex items-center justify-center gap-4">
-            <img src="src/assets/title-image.svg" alt="Minesweeper" className="w-16 h-16" />
+            <img src="title-image.svg" alt="Minesweeper" className="w-16 h-16" />
             <h1 className="text-5xl font-bold text-gray-800">Minesweeper</h1>
         </div>
     );
